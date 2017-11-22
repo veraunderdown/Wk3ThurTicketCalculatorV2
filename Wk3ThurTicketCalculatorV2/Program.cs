7bb32afb5f53293b50af4b8d0b656074924a3957
Oleg Gorecan <rvo@btinternet.com>
2017-11-22 17:08:06 +0000

    Add project files.

---
+++ Wk3ThurTicketCalculatorV2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3ThurTicketCalculatorV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //variables
+
+            int numAdults; 
+            int numChildren;
+            int numSeniors;
+            int numFamily;
+            int totalPrice;
+
+            //input
+
+            Console.Write("Enter the number of Adults: ");
+            numAdults = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of Children: ");
+            numChildren = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of Seniors: ");
+            numSeniors = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of Family Tickets: ");
+            numFamily = int.Parse(Console.ReadLine());
+
+            //process
+
+            totalPrice = numAdults * 1490 + numChildren * 990 + numSeniors * 990;
+
+            //output
+
+            Console.WriteLine("Total Price:" + totalPrice.ToString());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
